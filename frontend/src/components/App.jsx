3a9bbe4d32f3f6f57d6a537387b233f5000d6e97
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2022-01-29 12:22:31 -0600

    Created additional pages and tested page-switch functionality.

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,20 @@
 // import react, child component
 import React, { useState, useEffect, useContext } from 'react';
 import GlobalContext from '../contexts/context.js';
-import Login from './Login.jsx';
+import Login from './login/Login.jsx';
+import Register from './login/Register.jsx';
+import StoreView from './store/StoreView.jsx';
+import UserInfo from './login/UserInfo.jsx';
 
 
 const App = () => {
   const [page, setPage] = useState('login');
 
   let pages = {
-    login: <Login />
+    login: <Login />,
+    register: <Register />,
+    storeView: <StoreView />,
+    userInfo: <UserInfo />
   }
 
   return (
