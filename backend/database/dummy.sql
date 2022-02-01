3567f1e2b1b02f34070fe64f0428a8e49fdccf7b
mlawso33-code <marclawson195@gmail.com>
2022-01-31 18:05:54 -0800

    schema fixes

--- backend/database/dummy.sql
+++ backend/database/dummy.sql
@@ -1,8 +1,8 @@
 /* DUMMY STORE */
-INSERT INTO stores (name, street_address, city, state, zip, latitude, longitude, store_open, url)
-VALUES ('Coffee Test Shop', '555 Test Me Dr.', 'TestCity', 'TS', '12345', '5LAT','1LONG', 'Monday', 'google.com');
+INSERT INTO stores (name, street_address, city, state, zip, latitude, longitude, store_open, store_close, url)
+VALUES ('Coffee Test Shop', '555 Test Me Dr.', 'TestCity', 'TS', '12345', '5LAT','1LONG', '08:00:00', '19:00:00', 'google.com');
 
 
 /* DUMMY REVIEWS */
-INSERT INTO reviews (rating, body, date)
-VALUES (5, 'Weirdest coffeee ever, was all in code?', '01-31-2022')
+INSERT INTO reviews (user_id, store_id, rating, body, date)
+VALUES (1, 1, 5, 'Weirdest coffeee ever, was all in code?', '2022-01-31');
