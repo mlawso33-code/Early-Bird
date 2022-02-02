b188ad1d8bfdea7907b1a461d110bf7f00c775d3
zachfry <iitsfry@gmail.com>
2022-02-01 18:06:22 -0600

    merge

--- backend/database/index.js
+++ backend/database/index.js
@@ -6,24 +6,11 @@ require('dotenv').config();
 
 // create a connection to the db
 const connection = mysql.createConnection({
-<<<<<<< HEAD
   host: process.env.DATABASE_HOST_IP,
   port: process.env.DATABASE_PORT,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_CURRENT_DB
-=======
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'early_bird'
-  // host: process.env.DATABASE_HOST_IP,
-  // port: process.env.DATABASE_PORT,
-  // user: process.env.DATABASE_USER,
-  // password: process.env.DATABASE_PASSWORD,
-  // database: process.env.DATABASE_CURRENT_DB
->>>>>>> main
 });
 
 // actually connect
