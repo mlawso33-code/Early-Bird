1bb1d32471571e123bd62d52ca233d005c2a97c7
Sixto <sixtomgarcia7@gmail.com>
2022-02-03 18:39:12 -0500

    to and from locations for direction should render dynamically now

--- frontend/src/components/map/MapIndex.js
+++ frontend/src/components/map/MapIndex.js
@@ -8,26 +8,27 @@ import { GOOGLE_API_KEY } from '../../../../config.js'
 const G_API_URL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&&v=3.exp&libraries=geometry,drawing,places`;
 
 const MapIndex = () => {
-  const { userInfo, setUserInfo, storeData, setStoreData } = useContext(GlobalContext);
+  const { userInfo, setUserInfo, storeData, setStoreData, currStore, setCurrStore } = useContext(GlobalContext);
   const [fromObj, setFromObj] = useState({});
   const [toObj, setToObj] = useState({});
   const [defaultZoom, setDefaultZoom] = useState(15);
   const [map, setMap] = useState(null);
-  const [center, setCenter] = useState({ lat: 40.0326, lng: -105.2801 })
+  const [center, setCenter] = useState({})
 
   useEffect(() => {
     const { latitude: fromLat, longitude: fromLng, state: fromState } = userInfo;
-    //const {latitude: toLat, longitude: toLng, state: toState} = selectedStore;
-    setFromObj({ lat: fromLat, lng: fromLng, fromTitle: fromState });
-    //settoObj({lat: toLat, lng: toLng, toTitle: toState});
-    let centerLat = ((Number(fromLat) + Number(fromLng)) / 2);
-    //let centerLng = ((Number(toLat) + Number(toLng)) / 2);
-    //setCenter({lat: centerLat, lng: centerLng})
+    const { latitude: toLat, longitude: toLng, state: toState } = currStore;
 
-  }, [userInfo])
+    setFromObj({ lat: fromLat, lng: fromLng, fromTitle: fromState });
+    setToObj({ lat: toLat, lng: toLng, toTitle: toState });
+
+    let centerLat = ((Number(fromLat) + Number(fromLng)) / 2);
+    let centerLng = ((Number(toLat) + Number(toLng)) / 2);
+    setCenter({ lat: centerLat, lng: centerLng })
+
+  }, [userInfo, currStore])
 
   return (
-    console.log('FROMOBJ::: ', fromObj),
     < GoogleMap
       defaultZoom={12}
       center={center}
@@ -37,7 +38,7 @@ const MapIndex = () => {
         <DirectionRenderComponent
           strokeColor={"#921d25"}
           from={fromObj}
-          to={{ lat: '40.0502', lng: '-105.2892', toTitle: 'NY' }}
+          to={toObj}
         />
         : null
       }
